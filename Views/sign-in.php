b888ecca1a0462f0d730d7b56075055055659ff5
saneto <kaze.rge@icloud.com>
2021-09-15 17:32:05 +0900

    ログイン機能作成

--- Views/sign-in.php
+++ Views/sign-in.php
@@ -1,15 +1,8 @@
-<?php
-//設定関連を読み込む
-include_once('../config.php');
-//便利な関数を読み込む
-include_once('../util.php');
-?>
-
 <!DOCTYPE html>
 <html lang="ja">
  
 <head>
-    <?php include_once('../Views/common/head.php');?> 
+    <?php include_once('../Views/common/head.php'); ?>
     <title>ログイン画面 / Twitterクローン</title>
     <meta name="description" content="ログイン画面です">
 </head>
@@ -19,6 +12,13 @@ include_once('../util.php');
         <form action="sign-in.php" method="post">
             <img src="<?php echo HOME_URL; ?>Views/img/logo-white.svg" alt="" class="logo-white">
             <h1>Twitterクローンにログイン</h1>
+ 
+            <?php if (isset($view_try_login_result) && $view_try_login_result === false) : ?>
+                <div class="alert alert-warning text-sm" role="alert">
+                    ログインに失敗しました。メールアドレス、パスワードが正しいかご確認ください。
+                </div>
+            <?php endif; ?>
+ 
             <input type="email" class="form-control" name="email" placeholder="メールアドレス" required autofocus>
             <input type="password" class="form-control" name="password" placeholder="パスワード" required>
             <button class="w-100 btn btn-lg" type="submit">ログイン</button>
